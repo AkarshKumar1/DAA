170d104aec9512c04e3809fe088fc9b4f09c54c8
AkarshKumar1 <knot14hx8@gmail.com>
2025-09-25 09:53:24 +0530

    Create Floyd_Warshall.c

---
+++ DAA Codes/Floyd_Warshall.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define INF 1000000000   // A large value representing infinity
+#define MAX 100          // Maximum number of vertices
+
+int main() {
+    int V;
+    int dist[MAX][MAX];
+
+    // Input number of vertices
+    printf("Enter number of vertices: ");
+    scanf("%d", &V);
+
+    // Input adjacency matrix
+    printf("Enter the adjacency matrix (use %d for INF):\n", INF);
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &dist[i][j]);
+        }
+    }
+
+    // Floyd–Warshall Algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print shortest distance matrix
+    printf("\nShortest distances between every pair of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%3d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
